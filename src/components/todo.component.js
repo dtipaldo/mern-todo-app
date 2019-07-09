3fa44ae82d79937829d33f1db3df92548928a3e3
Dan Tipaldo <dtipaldo@gmail.com>
2019-07-09 08:29:25 -0400

    Edit Todo Implemented

--- src/components/todo.component.js
+++ src/components/todo.component.js
@@ -3,9 +3,17 @@ import { Link } from 'react-router-dom';
 
 export const Todo = props => (
     <tr>
-        <td>{props.todo.todo_description}</td>
-        <td>{props.todo.todo_responsible}</td>
-        <td>{props.todo.todo_priority}</td>
+        <td
+            className={props.todo.todo_completed ? 'completed' : ''}>
+            {props.todo.todo_description}
+        </td>
+        <td
+            className={props.todo.todo_completed ? 'completed' : ''}>
+            {props.todo.todo_responsible}
+        </td>
+        <td className={props.todo.todo_completed ? 'completed' : ''}>
+            {props.todo.todo_priority}
+        </td>
         <td>
             <Link to={"/edit/"+props.todo._id}>Edit</Link>
         </td>
